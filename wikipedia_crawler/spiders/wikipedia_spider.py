c2b38a213c77e0221b386be3b41105e7ba1dca59
hashiwa000 <gate000jp@gmail.com>
2019-08-21 15:41:05 +0900

    initial commit

---
+++ wikipedia_crawler/spiders/wikipedia_spider.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class WikipediaSpiderSpider(scrapy.Spider):
+    name = 'wikipedia_spider'
+    allowed_domains = ['https://ja.wikipedia.org/wiki/']
+    start_urls = ['https://ja.wikipedia.org/wiki/Wikipedia:ウィキポータル']
+
+    def parse(self, response):
+        pass
+	#for post in response.css('#mw-content-text'):
+        #    # items に定義した Post のオブジェクトを生成して次の処理へ渡す
+        #    yield Page(
+        #        url=post.css('div.post-header a::attr(href)').extract_first().strip(),
+        #        title=post.css('div.post-header a::text').extract_first().strip(),
+        #        date=post.css('div.post-header span.date a::text').extract_first().strip(),
+        #    )
